b87e9624374a8139172b6ad0f7459fd092c5dfd8
MariapBouAoun <73455465+MariaPBouAoun3@users.noreply.github.com>
2021-07-20 11:06:26 +0300

    Update Chapitre2CoreJava2.java

--- tp5/question2/Chapitre2CoreJava2.java
+++ tp5/question2/Chapitre2CoreJava2.java
@@ -2,7 +2,8 @@ package question2;
 
 import java.util.List;
 import java.util.Map;
-// à  compléter
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.StringTokenizer;
 
 public class Chapitre2CoreJava2 {
@@ -12,12 +13,12 @@ public class Chapitre2CoreJava2 {
      * 
      **/
     public static List<String> listeDesMots() {
-        List<String> liste = null; // à  compléter
-
+       List<String> liste = new LinkedList<String>();
         StringTokenizer st = new StringTokenizer(Chapitre2CoreJava2.CHAPITRE2,
                 "[](){};, :.\n\"");
-        // à  compléter
-
+        while (st.hasMoreTokens()) {
+            liste.add(st.nextToken());
+        }
         return liste;
     }
 
@@ -28,9 +29,16 @@ public class Chapitre2CoreJava2 {
      * @param liste la liste des mots
      */
     public static Map<String, Integer> occurrencesDesMots(List<String> liste) {
-        Map<String, Integer> table = null; // à  compléter
-        // à  compléter
-        // à  compléter
+        Map<String, Integer> table = new HashMap<String, Integer>();
+        for(String eltString : liste){
+            if(!table.containsKey(eltString)){
+                table.put(eltStr, 1);
+            }else {
+                Integer occDuMot = table.get(eltString);
+                occDuMot += 1;
+                table.put(eltString, occDuMot);
+            }
+        }
         return table;
     }
 
