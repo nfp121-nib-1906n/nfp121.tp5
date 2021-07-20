b29cea8f2ef265d643d1bec8d328646625fe724d
MariapBouAoun <73455465+MariaPBouAoun3@users.noreply.github.com>
2021-07-20 10:03:02 +0300

    Update Ensemble.java

--- tp5/question1/Ensemble.java
+++ tp5/question1/Ensemble.java
@@ -7,41 +7,61 @@ public class Ensemble<T> extends AbstractSet<T> {
 	protected java.util.Vector<T> table = new java.util.Vector<T>();
 
 	public int size() {
-		return table.size();
+		 if(table == null)
+            return 0;
+        return table.size();
 	}
 
 	public Iterator<T> iterator() {
-		return table.iterator();
+		   if(table == null)
+            return null;
+        return table.iterator();
 	}
 
 	public boolean add(T t) {
-		// à compléter pour la question1-1
-
-		return false;
+        if(table == null)
+            return false;
+        if(!this.contains(t)){ 
+            table.add(t); 
+            return true;
+        }
+        return false;
 	}
 
 	public Ensemble<T> union(Ensemble<? extends T> e) {
-		// à compléter pour la question1-2
-
-		return null;
+         if(e == null || this.table == null)
+            return null;
+        Ensemble res = new Ensemble();
+        res.addAll(this.table);
+        res.addAll(e.table);
+        return res;
 	}
 
 	public Ensemble<T> inter(Ensemble<? extends T> e) {
-		// à compléter pour la question1-2
-
-		return null;
+        if(e == null || this.table == null)
+        return null;
+        Ensemble res = new Ensemble();
+        res.addAll(this.table);
+        res.retainAll(e.table);
+        return res;
 	}
 
 	public Ensemble<T> diff(Ensemble<? extends T> e) {
-		// à compléter pour la question1-2
-
-		return null;
+	if(e == null || this.table == null)
+        return null;
+        Ensemble res = new Ensemble();
+        res.addAll(this.table);
+        res.removeAll(e.table);
+        return res;
 	}
 
 	Ensemble<T> diffSym(Ensemble<? extends T> e) {
-		// à compléter pour la question1-2
-
-		return null;
+        Ensemble union = this.union(e);
+        if (union == null){
+            return null;
+        }
+        Ensemble res = this.union(e).diff(this.inter(e));
+        return res;
 	}
 	
 }
