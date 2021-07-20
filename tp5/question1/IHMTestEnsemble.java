ea048429e81b6e88a0275a1bdb92949c425a291d
MariapBouAoun <73455465+MariaPBouAoun3@users.noreply.github.com>
2021-07-20 10:56:21 +0300

    Update IHMTestEnsemble.java

--- tp5/question1/IHMTestEnsemble.java
+++ tp5/question1/IHMTestEnsemble.java
@@ -71,42 +71,42 @@ public class IHMTestEnsemble extends JFrame {
         button1 = new JButton("union");
         button1.setBackground(java.awt.Color.red);
         button1.setName("union");
-        button1.addActionListener(null  // à compléter, par une instance de
-                                        // classe anonyme, usage de
-                                        // unionActionPerformed voir en bas de
-                                        // page
-        );
+        button1.addActionListener(new ActionListener(){
+                public void actionPerformed(ActionEvent ae){
+                    unionActionPerformed(ae);
+                }
+            });
 
         panel3.add(button1);
         button2 = new JButton("intersection");
         button2.setBackground(java.awt.Color.yellow);
         button2.setName("intersection");
-        button2.addActionListener(null  // à compléter, par une instance de
-                                        // classe anonyme,
-                                        // intersectionActionPerformed voir en
-                                        // bas de page
-        );
-
+        button2.addActionListener(new ActionListener(){
+                public void actionPerformed(ActionEvent ae){
+                    intersectionActionPerformed(ae);
+                }
+            });
         panel3.add(button2);
         button3 = new JButton("difference");
         button3.setBackground(java.awt.Color.pink);
         button3.setActionCommand("difference");
         button3.setName("difference");
-        button3.addActionListener(null  // à compléter, par une instance de
-                                        // classe anonyme, usage de
-                                        // differenceActionPerformed voir en bas
-                                        // de page
-        );
+        button3.addActionListener(new ActionListener(){
+                public void actionPerformed(ActionEvent ae){
+                    differenceActionPerformed(ae);
+                }
+            });
+
 
         panel3.add(button3);
         button4 = new JButton("diffSymetrique");
         button4.setBackground(java.awt.Color.cyan);
         button4.setName("diffSymetrique");
-        button4.addActionListener(null  // à compléter, par une instance de
-                                        // classe anonyme, usgae de
-                                        // diffSymetriqueActionPerformed voir en
-                                        // bas de page
-        );
+        button4.addActionListener(new ActionListener(){
+                public void actionPerformed(ActionEvent ae){
+                    diffSymetriqueActionPerformed(ae);
+                }
+            });
 
         panel3.add(button4);
         add(panel3);
